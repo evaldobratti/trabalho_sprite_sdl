ad5817a634ecaab6663d3a139330e848c7fcb1b7
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-05-23 21:21:06 -0300

    ataque + som, sprite de ataque deslocado e não está animando na posição correta

--- sprite/main.cpp
+++ sprite/main.cpp
@@ -4,7 +4,135 @@
 #include <stdio.h>
 #include <SDL_mixer.h>
 #include <vector>
-#include "sprite.cpp"
+using namespace std;
+
+class Frame {
+public:
+	int getX(){
+		return this->x;
+	}
+	int getY() {
+		return this->y;
+	}
+	int getW() {
+		return this->w;
+	}
+	int getH() {
+		return this->h;
+	}
+	bool isPosicaoRelativaAoCentro() {
+		return this->posicaoRelativaAoCentro;
+	}
+	Frame(int x, int y, int w, int h){
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+		posicaoRelativaAoCentro = true;
+	}
+	Frame(int x, int y, int w, int h, bool posicaoRelativaAoCentro){
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+		this->posicaoRelativaAoCentro = posicaoRelativaAoCentro;
+	}
+private:
+	int x, y, w, h;
+	bool posicaoRelativaAoCentro;
+};
+
+
+class FrameSet {
+public:
+	FrameSet() {
+		i = 0;
+		nextFrameSet = NULL;
+	}
+	FrameSet(FrameSet *nextFrameSet) {
+		vector<Frame> temp;
+		this->frames = temp;
+		this->nextFrameSet = nextFrameSet;
+	}
+	vector<Frame> getFrames(){
+		return this->frames;
+	}
+	void addFrame(Frame frame) {
+		this->frames.push_back(frame);
+	}
+	Frame getFrameAtual() {
+		i += 1;
+		if (i >= this->frames.size()) {
+			if (nextFrameSet != NULL)
+				return nextFrameSet->getFrameAtual();
+			else
+				i = 0;
+		}
+		return frames.at(i);
+	}
+	void setNextFrameSet(FrameSet* nextFrameSet) {
+		this->nextFrameSet = nextFrameSet;
+	}
+	FrameSet& getNextFrameSet() {
+		if (nextFrameSet != NULL){
+			return *nextFrameSet;
+		}
+		return *this;
+	}
+	void reset() {
+		if (nextFrameSet != NULL)
+			nextFrameSet->reset();
+		i = 0;
+
+	}
+private:
+	int i;
+	vector<Frame> frames;
+	FrameSet *nextFrameSet;
+	Frame *frameAtual;
+
+	bool isUltimoFrame(int i) {
+		return i >= frames.size();
+	}
+};
+
+class Sprite {
+public:
+	void setFrameSetAtacando(FrameSet* a) {
+		atacando = a;
+	}
+	void setFrameSetCaminhando(FrameSet* c) {
+		caminhando = c;
+	}
+	void setFrameSetParado(FrameSet* p) {
+		parado = p;
+	}
+	void setStatusCaminhando() {
+		if (atual == parado) {
+			atual->reset();
+
+			atual = caminhando;
+		}
+	}
+	void setStatusParado() {
+		atual->reset();
+		atual = parado;
+	}
+	void setStatusAtacando() {
+		atual->reset();
+		atual = atacando;
+	}
+	Frame getFrameAtual() {
+		if (atual == NULL)
+			atual = parado;
+		return atual->getFrameAtual();
+	}
+private:
+	FrameSet* caminhando;
+	FrameSet* parado;
+	FrameSet* atual;
+	FrameSet* atacando;
+};
 
 FrameSet *loadFramesCaminhando() {
 	FrameSet* caminhando = new FrameSet();
@@ -50,7 +178,28 @@ FrameSet* loadFramesIniciandoCaminhada() {
 	return iniciandoCaminhada;
 }
 
-using namespace std;
+FrameSet* loadFramesAtacando() {
+	FrameSet* atacando = new FrameSet();
+	atacando->addFrame(Frame(92, 137, 41, 49));
+	atacando->addFrame(Frame(136, 137, 41, 49));
+	atacando->addFrame(Frame(185, 127, 54, 59));
+	atacando->addFrame(Frame(244, 123, 95, 63));
+	atacando->addFrame(Frame(343, 123, 101, 71));
+	atacando->addFrame(Frame(343, 123, 101, 71));
+	atacando->addFrame(Frame(451, 133, 102, 62));
+	atacando->addFrame(Frame(74, 211, 100, 51));
+	atacando->addFrame(Frame(180, 211, 70, 51));
+	atacando->addFrame(Frame(258, 211, 50, 46));
+	atacando->addFrame(Frame(317, 211, 48, 44));
+	atacando->addFrame(Frame(373, 211, 48, 42));
+	atacando->addFrame(Frame(427, 211, 48, 42));
+	atacando->addFrame(Frame(483, 206, 39, 47));
+	atacando->addFrame(Frame(532, 205, 39, 48));
+
+	return atacando;
+
+}
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int VELOCITY = 8;
@@ -73,17 +222,23 @@ SDL_Surface* megamanSprite = NULL;
 SDL_Surface* fundoBranco = NULL;
 
 Mix_Chunk *wind = NULL;
+Mix_Chunk *ataque = NULL;
 
 FrameSet *parado = loadFramesParado();
 FrameSet *caminhando = loadFramesCaminhando();
+FrameSet *atacando = loadFramesAtacando();
 FrameSet *iniciandoCaminhada = loadFramesIniciandoCaminhada();
 
-FrameSet *atual = parado;
+Sprite *sprite = new Sprite();
 
 bool init() {
-	iniciandoCaminhada->setNextFrameSet(caminhando);
-	atual = parado;
+	atacando->setNextFrameSet(parado);
 
+	iniciandoCaminhada->setNextFrameSet(caminhando);
+	sprite->setFrameSetCaminhando(iniciandoCaminhada);
+	sprite->setFrameSetParado(parado);
+	sprite->setFrameSetAtacando(atacando);
+	
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("Erro ao inicializar o vídeo! %s", SDL_GetError());
 		return false;
@@ -118,10 +273,8 @@ bool loadMedia() {
 	fundoBranco = SDL_ConvertSurfaceFormat(spriteSurface, SDL_PIXELFORMAT_RGBA8888, 0);
 	SDL_FreeSurface(spriteSurface);
 	
+	ataque = Mix_LoadWAV("C:/pos-jogos/sprite/sprite/sprite/47125__gyzhor__lightsaber3.wav");
 	wind = Mix_LoadWAV("C:/pos-jogos/sprite/sprite/sprite/footsteps.wav");
-	if (wind == NULL) {
-		printf("erro abrindo arquivo %s", Mix_GetError());
-	}
 	return true;
 }
 
@@ -152,35 +305,33 @@ int main(int argc, char* args[])
 						quit = true;
 					}
 					if (e.type == SDL_KEYDOWN) {
-						if (soundTimeout < 0) {
-							soundTimeout = 39;
-							Mix_PlayChannel(-1, wind, 0);
-						}
 						if (e.key.keysym.sym == SDLK_ESCAPE)
 							quit = true;
 						
-						if (atual == parado)
-							atual = iniciandoCaminhada;
-						
-						if (e.key.keysym.sym == SDLK_RIGHT)
+						if (e.key.keysym.sym == SDLK_SPACE) {
+							sprite->setStatusAtacando();
+							Mix_PlayChannel(-1, ataque, 0);
+						}
+						if (e.key.keysym.sym == SDLK_RIGHT) {
+							if (soundTimeout < 0) {
+								soundTimeout = 39;
+								Mix_PlayChannel(-1, wind, 0);
+							}
+							sprite->setStatusCaminhando();
 							bodyX = bodyX + VELOCITY;
+						}
 						else if (e.key.keysym.sym == SDLK_LEFT)
 							bodyX = bodyX - VELOCITY;
 
-						
-						
 					} 
 					if (e.type == SDL_KEYUP) {
-						atual->reset();
-						atual = parado;
+						sprite->setStatusParado();
 					}
 				}
 				
 				if (soundTimeout >= 0)
 					soundTimeout -= 1;
-				printf("%i\n", soundTimeout);
-				Frame frameAtual = atual->getFrameAtual();
-				atual = &(atual->getNextFrameSet());
+				Frame frameAtual = sprite->getFrameAtual();
 				SDL_Rect source;
 				source.x = frameAtual.getX();
 				source.y = frameAtual.getY();
@@ -188,8 +339,15 @@ int main(int argc, char* args[])
 				source.h = frameAtual.getH();
 
 				SDL_Rect destiny;
-				destiny.x = bodyX + bodyH - source.h;
-				destiny.y = bodyY + (bodyW - source.w) / 2;
+				
+				if (frameAtual.isPosicaoRelativaAoCentro()) {
+					destiny.x = bodyX;
+					destiny.y = bodyY;
+				}
+				else {
+					destiny.x = bodyX;
+					destiny.y = bodyY;
+				}
 
 				SDL_BlitSurface(fundoBranco, NULL, mainSurface, NULL);
 				SDL_BlitSurface(megamanSprite, &source, mainSurface, &destiny);
